66e9befc3359d5df38c82f9ef519ff59b694711c
mahesh1706 <immaheshwaran17@gmail.com>
2022-10-09 05:25:10 -0700

    fact in

---
+++ fact.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,fact;
+    fact=1;
+    printf("Enter a number:");
+    scanf("%d",n);
+    for (i=i; i<=n; ++i)
+    {
+      fact=fact*i;  /* code */
+    }
+    printf("Fact of num %d",fact);
+    return 0;
+    
+}
